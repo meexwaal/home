e3342a39be74c101f0c6542e2c418c3935260ce3
Maxwell Johnson <maximusjohnsonus@gmail.com>
2021-02-28 21:06:36 -0800

    Sync diverged branches

--- hook.sh
+++ hook.sh
@@ -17,7 +17,16 @@ cd $HOME_DIR
 # If the local checkout is unmodified, pull. Otherwise, issue a warning.
 if git diff-index --quiet HEAD --
 then
-    git pull
+    git fetch -q
+    # Only update if there are remote changes.
+    if ! git diff-index --quiet origin/master --
+    then
+        >&2 printf "Updating $HOME_DIR... "
+        git pull --quiet --ff-only && \
+            >&2 echo "done."
+    fi
 else
     >&2 echo "You have local changes to $HOME_DIR. Commit them you wanker."
 fi
+
+cd - > /dev/null
